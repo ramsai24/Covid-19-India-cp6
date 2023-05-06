ddbe60c678d4326ee1eda9e186b74c0f20304f41
ramsai24 <jnrsai24@gmail.com>
2023-05-06 19:43:31 +0000

    ten cases are pending 2 pending

--- app.js
+++ app.js
@@ -44,3 +44,151 @@ app.get("/states/", async (request, response) => {
   let stateList = await db.all(sqlQuery);
   response.send(stateList.map((eachObject) => convertSnakeToCamel(eachObject)));
 });
+
+//API 2
+
+app.get("/states/:stateId", async (request, response) => {
+  let { stateId } = request.params;
+
+  const sqlQuery = `
+    SELECT * 
+    FROM state 
+    WHERE 
+        state_id = ${stateId};`;
+
+  const stateDetails = await db.get(sqlQuery);
+  response.send(convertSnakeToCamel(stateDetails));
+});
+
+//API 3
+
+app.post("/districts/", async (request, response) => {
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+
+  const sqlQuery = `
+    INSERT INTO 
+        district ( 
+            district_name,
+            state_id,
+            cases,
+            cured,
+            active,
+            deaths)
+    VALUES (
+        '${districtName}',
+        ${stateId},
+        ${cases},
+        ${cured},
+        ${active},
+        ${deaths}); `;
+
+  const updatedList = await db.run(sqlQuery);
+  response.send("District Successfully Added");
+  //response.send([{ id: updatedList.lastID }, "District Successfully Added"]);
+});
+
+//API 4
+
+const convertSnakeToCamel1 = (dbObject) => {
+  return {
+    districtId: dbObject.district_id,
+    districtName: dbObject.district_name,
+    stateId: dbObject.state_id,
+    cases: dbObject.cases,
+    cured: dbObject.cured,
+    active: dbObject.active,
+    death: dbObject.death,
+  };
+};
+
+app.get("/districts/:districtId/", async (request, response) => {
+  let { districtId } = request.params;
+
+  const sqlQuery = `
+    SELECT * 
+    FROM district 
+    WHERE 
+        district_id = ${districtId};`;
+
+  const districtDetails = await db.get(sqlQuery);
+  response.send(convertSnakeToCamel1(districtDetails));
+});
+
+//API 5
+app.delete("/districts/:districtId/", async (request, response) => {
+  let { districtId } = request.params;
+
+  const sqlQuery = `DELETE FROM district
+    WHERE district_id = ${districtId};`;
+
+  await db.run(sqlQuery);
+  response.send("District Removed");
+});
+
+//API 6
+app.put("/districts/:districtId/", async (request, response) => {
+  let { districtName, stateId, cases, cured, active, deaths } = request.body;
+  const { districtId } = request.params;
+  console.log(districtId);
+  const sqlQuery = ` 
+  UPDATE district
+  SET 
+    district_name = '${districtName}',
+    state_id = ${stateId},
+    cases = ${cases},
+    cured = ${cured},
+    active = ${active},
+    deaths = ${deaths}
+    WHERE 
+        district_id = ${districtId};`;
+
+  const updatedList = await db.run(sqlQuery);
+  response.send("District Details Updated");
+});
+
+//API 7
+
+const convertSnakeToCamel2 = (dbObject) => {
+  return {
+    totalCases: dbObject.cases,
+    totalCured: dbObject.cured,
+    totalActive: dbObject.active,
+    totalDeath: dbObject.deaths,
+  };
+};
+
+app.get("/states/:stateId/stats/", async (request, response) => {
+  let { stateId } = request.params;
+
+  const sqlQuery = `
+    SELECT  cases, cured, active, deaths
+    FROM district 
+    WHERE 
+        state_id = ${stateId};`;
+  //SUM(cases), SUM(cured),SUM(active), SUM(deaths)
+  const districtDetails = await db.get(sqlQuery);
+  response.send(convertSnakeToCamel2(districtDetails));
+});
+
+//API 8
+
+const convertSnakeToCamel3 = (dbObject) => {
+  return {
+    stateName: dbObject.state_name,
+  };
+};
+
+app.get("/districts/:districtId/details/", async (request, response) => {
+  let { districtId } = request.params;
+
+  const sqlQuery = `
+    SELECT  state_name
+    FROM district INNER JOIN state ON  district.state_id = state.state_id
+    WHERE 
+        district_id = ${districtId};`;
+  //SUM(cases), SUM(cured),SUM(active), SUM(deaths)
+  const districtDetails = await db.get(sqlQuery);
+  response.send(convertSnakeToCamel3(districtDetails));
+});
+
+module.exports = app;
